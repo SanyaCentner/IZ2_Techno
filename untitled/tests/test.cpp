dde04b9af2cd18c1e7fd37078a197eada6a5f8c9
Александр <aleksandr@MacBook-Air-Aleksandr.local>
2021-11-09 02:13:41 +0300

    First

--- untitled/tests/test.cpp
+++ untitled/tests/test.cpp
@@ -29,27 +29,27 @@ TEST(test_sequential, test1) {
     ASSERT_STREQ(result_sequential, "CaqJjZ@rambler.ru\n");
 }
 
-TEST(test_parallel, test2) {
-    char result_parallel[25] = {};
-    const char *file_with_mailers;
-    file_with_mailers = "mailer_test_1.txt";
-    const char *file_with_names;
-    file_with_names = "names_test_1.txt";
-    const char *file_with_recipients;
-    file_with_recipients = "recipients_test_1.txt";
-    const char *file_with_topic;
-    file_with_topic = "topic_test_1.txt";
-    const char *file_with_letter;
-    file_with_letter = "letter_test_1.txt";
-    const char *file_with_date;
-    file_with_date = "date_test_1.txt";
-    char date_start[11] = "2012.1.1";
-    char date_finish[11] = "2021.1.1";
-    parallel(file_with_names, file_with_mailers, file_with_recipients, file_with_topic,
-             file_with_letter, file_with_date, result_parallel, 32,
-             10, date_start, date_finish);
-    ASSERT_STREQ(result_parallel, "CaqJjZ@rambler.ru\n");
-}
+//TEST(test_parallel, test2) {
+//    char result_parallel[25] = {};
+//    const char *file_with_mailers;
+//    file_with_mailers = "mailer_test_1.txt";
+//    const char *file_with_names;
+//    file_with_names = "names_test_1.txt";
+//    const char *file_with_recipients;
+//    file_with_recipients = "recipients_test_1.txt";
+//    const char *file_with_topic;
+//    file_with_topic = "topic_test_1.txt";
+//    const char *file_with_letter;
+//    file_with_letter = "letter_test_1.txt";
+//    const char *file_with_date;
+//    file_with_date = "date_test_1.txt";
+//    char date_start[11] = "2012.1.1";
+//    char date_finish[11] = "2021.1.1";
+//    parallel(file_with_names, file_with_mailers, file_with_recipients, file_with_topic,
+//             file_with_letter, file_with_date, result_parallel, 32,
+//             10, date_start, date_finish);
+//    ASSERT_STREQ(result_parallel, "CaqJjZ@rambler.ru\n");
+//}
 
 TEST(test_comparison, test3) {
 char data_start[12] = "2014.9.9";
